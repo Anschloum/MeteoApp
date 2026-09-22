3b487ff84a4149b0d7b2534efad1837d0f5dd39f
Anschloum <jallageas97@gmail.com>
2026-09-22 15:38:53 +0200

    Refactor TokenResponse to use Kotlin property names

--- MeteoApp/app/src/main/java/com/exemple/meteo/MeteoFranceAuthService.kt
+++ MeteoApp/app/src/main/java/com/exemple/meteo/MeteoFranceAuthService.kt
@@ -1,6 +1,5 @@
 package com.exemple.meteo
 
-import com.google.gson.annotations.SerializedName
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,16 +8,13 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Header
 import retrofit2.http.POST
 
-// Modèle de réponse du serveur d'authentification
 data class TokenResponse(
-    @SerializedName("access_token") val accessToken: String,
-    @SerializedName("token_type") val tokenType: String,
-    @SerializedName("expires_in") val expiresIn: Long
+    val access_token: String,
+    val token_type: String,
+    val expires_in: Long
 )
 
-// Service Retrofit dédié au serveur de jetons
 interface MeteoFranceAuthService {
-
     @FormUrlEncoded
     @POST("token")
     suspend fun fetchToken(
